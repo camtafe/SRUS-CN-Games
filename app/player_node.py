d4c7af38f3a820e0346ea432b67ee419feb6d49e
cam <camnorie@gmail.com>
2024-08-19 23:02:06 +0800

    step 4 player class

---
+++ app/player_node.py
@@ -0,0 +1,46 @@
+class PlayerNode:
+    def __init__(self, player):
+        self.__player = player
+        self.__next = None
+        self.__prev = None
+
+
+# getter for __player
+@property
+def player(self):
+    return self.__player
+
+
+# getter for __next
+@property
+def next(self):
+    return self.__next
+
+
+# getter for __prev
+@property
+def prev(self):
+    return self.__prev
+
+
+# setter for next
+@next.setter
+def next(self, node):
+    self.__next = node
+
+
+# setter for prev
+@prev.setter
+def prev(self, node):
+    self.__prev = node
+
+
+# returns the uid of the player object
+@property
+def key(self):
+    return self.__player.uid
+
+
+# lists the variable data inside the player node in string form
+def __str__(self):
+    return f"Player Node(Name:{self.__player.name}, uid:{self.key})"
