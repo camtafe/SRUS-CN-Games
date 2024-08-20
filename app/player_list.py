dee686603d411b5105cf13d45b733d2d1c792a53
cam <camnorie@gmail.com>
2024-08-21 01:01:06 +0800

    step 5: changing test cases, renaming variables

--- app/player_list.py
+++ app/player_list.py
@@ -1,6 +1,17 @@
 class PlayerList:
-    def __init__(self, r=None):
+    def __init__(self):
         self.__head = None
+        self.__tail = None
 
     def is_empty(self):
         return self.__head is None
+
+    def insert_at_head(self, node):
+        if self.is_empty:
+            # empty so new node becomes head and tail
+            self.__head = node
+            self.__tail = node
+        else:
+            node.next = self.__head
+            self.__head.prev = node
+            self.__head = node
--- test/python_list_test.py
+++ test/python_list_test.py
@@ -1,7 +1,16 @@
 import unittest
 from player_list import PlayerList
+from player_node import PlayerNode
+from player import Player
 
 class TestList(unittest.TestCase):
     def test_empty_list(self):
-        list0 = PlayerList()
-        self.assertEqual(list0.is_empty(), True)
+        empty_list = PlayerList()
+        self.assertTrue(empty_list.is_empty())
+
+    def test_filled_list(self):
+        filled_list = PlayerList()
+        filled_player = Player("513", "Mark")
+        node = PlayerNode(filled_player)
+        filled_list.insert_at_head(node)
+        self.assertFalse(filled_list.is_empty())
