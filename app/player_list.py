581bf3b484ee50372c0c143dc0391ea6e1f60ceb
cam <camnorie@gmail.com>
2024-08-21 19:41:11 +0800

    step 5: tail insertion commenting

--- app/player_list.py
+++ app/player_list.py
@@ -7,7 +7,7 @@ class PlayerList:
         return self.__head is None
 
     def insert_at_head(self, node):
-        if self.is_empty:
+        if self.is_empty():
             # empty so new node becomes head and tail
             self.__head = node
             self.__tail = node
@@ -15,3 +15,17 @@ class PlayerList:
             node.next = self.__head
             self.__head.prev = node
             self.__head = node
+
+    def insert_at_tail(self, node):
+        if self.is_empty():
+            # empty so new node becomes head and tail
+            self.__head = node
+            self.__tail = node
+        else:
+            # tail marks next node as the new node
+            self.__tail.next = node
+            # the new node points to the current tail as its previous node
+            node.prev = self.__tail
+            # the node then marks itself as the tail
+            # the old tail still exists but is no longer designated as the tail
+            self.__tail = node
