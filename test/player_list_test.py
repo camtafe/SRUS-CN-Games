86f86f159ea994f2b3f0d9b95d8e05219ef74f4c
cam <camnorie@gmail.com>
2024-08-22 23:12:03 +0800

    player testing optimizations, added key test, needs expected string outcome

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -4,10 +4,16 @@ from player_node import PlayerNode
 from player import Player
 
 
+first_player = Player("000513", "Mark")
+second_player = Player("000232", "Eugene")
+
+
 class TestList(unittest.TestCase):
+    def setUp(self):
+        self.test_list = PlayerList()
+
     def test_empty_list(self):
-        empty_list = PlayerList()
-        self.assertTrue(empty_list.is_empty())
+        self.assertTrue(self.test_list.is_empty())
 
     def test_removal_empty(self):
         empty_list = PlayerList()
@@ -15,98 +21,99 @@ class TestList(unittest.TestCase):
         self.assertIsNone(result)
 
     def test_head_insert_list(self):
-        test_list = PlayerList()
-        dummy_player = Player("000513", "Mark")
-        node = PlayerNode(dummy_player)
-        test_list.insert_at_head(node)
-        self.assertFalse(test_list.is_empty())
+        node = PlayerNode(first_player)
+        self.test_list.insert_at_head(node)
+        self.assertFalse(self.test_list.is_empty())
 
     def test_tail_insert_list(self):
-        test_list = PlayerList()
-        dummy_player = Player("000513", "Mark")
-        node = PlayerNode(dummy_player)
-        test_list.insert_at_tail(node)
-        self.assertFalse(test_list.is_empty())
+        node = PlayerNode(first_player)
+        self.test_list.insert_at_tail(node)
+        self.assertFalse(self.test_list.is_empty())
 
     def test_head_delete_single(self):
-        # create a list
-        test_list = PlayerList()
-        # create a test player
-        dummy_player = Player("000513", "Mark")
         # create a test node using the test player
-        dummy_node = PlayerNode(dummy_player)
+        dummy_node = PlayerNode(first_player)
         # insert the test node at the head of the list
-        test_list.insert_at_head(dummy_node)
+        self.test_list.insert_at_head(dummy_node)
         # remove the inserted node from the list
-        test_list.delete_from_head()
+        self.test_list.delete_from_head()
         # check the list is empty post removal
-        self.assertTrue(test_list.is_empty())
+        self.assertTrue(self.test_list.is_empty())
 
     def test_head_delete_multiple(self):
-        # prepare scenario with two nodes put in via head insertion
-        test_list = PlayerList()
-        first_player = Player("000513", "Mark")
-        second_player = Player("000232", "Eugene")
+        # prepare scenario with two nodes via head insertion
         first_node = PlayerNode(first_player)
         second_node = PlayerNode(second_player)
-        test_list.insert_at_head(first_node)
-        test_list.insert_at_head(second_node)
+        self.test_list.insert_at_head(first_node)
+        self.test_list.insert_at_head(second_node)
         # archive the old head
-        old_head = test_list.get_head()
+        old_head = self.test_list.get_head()
         # remove the head node
-        test_list.delete_from_head()
+        self.test_list.delete_from_head()
         # archive the new head
-        new_head = test_list.get_head()
-        self.assertFalse(test_list.is_empty())
+        new_head = self.test_list.get_head()
+        self.assertFalse(self.test_list.is_empty())
         self.assertEqual(new_head, first_node)
         self.assertNotEqual(new_head, old_head)
 
     def test_tail_delete_single(self):
         # create test scenario
-        test_list = PlayerList()
-        dummy_player = Player("000513", "Mark")
-        dummy_node = PlayerNode(dummy_player)
+        dummy_node = PlayerNode(first_player)
         # insert tail end node
-        test_list.insert_at_tail(dummy_node)
-        test_list.delete_from_head()
+        self.test_list.insert_at_tail(dummy_node)
+        self.test_list.delete_from_head()
         # check list is empty
-        self.assertTrue(test_list.is_empty())
+        self.assertTrue(self.test_list.is_empty())
 
     def test_tail_delete_multiple(self):
-        # prepare scenario with two nodes put in via tail insertion
-        test_list = PlayerList()
-        first_player = Player("000513", "Mark")
-        second_player = Player("000232", "Eugene")
+        # prepare scenario with two nodes via tail insertion
         first_node = PlayerNode(first_player)
         second_node = PlayerNode(second_player)
-        test_list.insert_at_tail(first_node)
-        test_list.insert_at_tail(second_node)
+        self.test_list.insert_at_tail(first_node)
+        self.test_list.insert_at_tail(second_node)
         # archive the old tail
-        old_tail = test_list.get_tail()
+        old_tail = self.test_list.get_tail()
         # remove the tail node
-        test_list.delete_from_tail()
+        self.test_list.delete_from_tail()
         # archive the new tail
-        new_tail = test_list.get_tail()
-        self.assertFalse(test_list.is_empty())
+        new_tail = self.test_list.get_tail()
+        self.assertFalse(self.test_list.is_empty())
         self.assertEqual(new_tail, first_node)
         self.assertNotEqual(new_tail, old_tail)
 
-    def test_delete_node_via_key(self):
-        # prepare scenario with two nodes put in via tail insertion
-        test_list = PlayerList()
-        first_player = Player("000513", "Mark")
-        second_player = Player("000232", "Eugene")
+    def test_delete_node_via_key_multiple(self):
+        # prepare scenario with two nodes via tail insertion
         first_node = PlayerNode(first_player)
         second_node = PlayerNode(second_player)
-        test_list.insert_at_tail(first_node)
-        test_list.insert_at_tail(second_node)
+        self.test_list.insert_at_tail(first_node)
+        self.test_list.insert_at_tail(second_node)
         # archive the old tail
-        old_tail = test_list.get_tail()
+        old_tail = self.test_list.get_tail()
         # remove the tail node
-        test_list.delete_via_key("000232")
+        self.test_list.delete_via_key("000232")
         # archive the new tail
-        new_tail = test_list.get_tail()
-        self.assertFalse(test_list.is_empty())
+        new_tail = self.test_list.get_tail()
+        self.assertFalse(self.test_list.is_empty())
         self.assertEqual(new_tail, first_node)
         self.assertNotEqual(new_tail, old_tail)
 
+    def test_delete_node_via_key_single(self):
+        # create test scenario
+        dummy_node = PlayerNode(first_player)
+        # insert tail end node
+        self.test_list.insert_at_tail(dummy_node)
+        # delete via player key
+        self.test_list.delete_via_key("000513")
+        # check list is empty
+        self.assertTrue(self.test_list.is_empty())
+
+    def display_test(self):
+        # prepare scenario with two nodes put in via head insertion
+        first_node = PlayerNode(first_player)
+        second_node = PlayerNode(second_player)
+        self.test_list.insert_at_head(first_node)
+        self.test_list.insert_at_head(second_node)
+        # display the lists using both possible functionalities
+        # true = forwards from head, tail = backwards from tail
+        self.test_list.display_lists()
+        self.test_list.display_lists(forward=False)
