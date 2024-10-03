092e42249a8602e8fd31df497a4129f44be80aab
cam <camnorie@gmail.com>
2024-10-04 00:38:55 +0800

    changed test function

--- test/hash_map_test.py
+++ test/hash_map_test.py
@@ -4,13 +4,6 @@ from app.player_node import PlayerNode
 from app.player import Player
 from app.hash_map import PlayerHashMap
 
-# dummy/test players and nodes for redundancy purposes
-first_player = Player("000513", "Mark")
-second_player = Player("000232", "Eugene")
-third_player = Player("000747", "Darius")
-first_node = PlayerNode(first_player)
-second_node = PlayerNode(second_player)
-third_node = PlayerNode(third_player)
 
 class TestCase(unittest.TestCase):
     def setUp(self):
